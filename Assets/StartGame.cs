6fea5a0517908901c04b7041074d2e569ec58ac5
sensei_himanshu <himanshu@bitcs.in>
2019-11-10 02:15:12 +0530

    Added Functionalities of KBC

---
+++ Assets/StartGame.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] Text question;
+    [SerializeField] Text option1;
+    [SerializeField] Text option2;
+    [SerializeField] Text option3;
+    [SerializeField] Text option4;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        question.text = state.getStateQuestion();
+        option1.text = state.getStateOptions()[0];
+        option2.text = state.getStateOptions()[1];
+        option3.text = state.getStateOptions()[2];
+        option4.text = state.getStateOptions()[3];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        int correctOption = -1;
+        int userAnswer = -1;
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (state.getCorrectAnswer() == state.getStateOptions()[i])
+            {
+                correctOption = i;
+                break;
+            }
+        }
+        // Debug.Log(correctOption);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            userAnswer = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            userAnswer = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            userAnswer = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            userAnswer = 3;
+        }
+
+        if (correctOption == userAnswer)
+        {
+            // if (state.getNextStates())
+            // {
+            Debug.Log("Correct" + state.getNextStates()[0]);
+            state = state.getNextStates()[0];
+            question.text = state.getStateQuestion();
+            option1.text = state.getStateOptions()[0];
+            option2.text = state.getStateOptions()[1];
+            option3.text = state.getStateOptions()[2];
+            option4.text = state.getStateOptions()[3];
+            // }
+        }
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(4, 5)] [SerializeField] string question;
+
+    [SerializeField] string[] options = new string[4];
+    [SerializeField] string correctOption;
+    [SerializeField] State[] nextStates;
+
+    public string getStateQuestion()
+    {
+        return question;
+    }
+
+    public string getCorrectAnswer()
+    {
+        return correctOption;
+    }
+
+    public string[] getStateOptions(){
+        return options;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+
+}
